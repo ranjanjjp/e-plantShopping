d308dad100e1ff8b620b3910b36bac93b7e40e20
ranjanjjp <ranjankumarsahujjp@gmail.com>
2025-07-14 16:01:08 -0400

    dispatch issues

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    const dispatch = useDispatch();
     const CartItems = useSelector(state => state.cart.items);
 
 
